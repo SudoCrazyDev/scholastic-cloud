36a770144916b411a6488ca1ca90d0a7b2d22309
philip-calub <philip.calub@adaca.com>
2026-03-10 15:10:37 +0800

    Added Title

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+document.title = import.meta.env.VITE_APP_TITLE ?? 'Scholastic Cloud'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
