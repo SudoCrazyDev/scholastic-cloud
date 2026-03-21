d500614fda1ae52f9efbc8ea7c683b51788bbb64
Philip Calub <philip@matterdesign.com.au>
2026-03-22 02:03:03 +0800

    Added Google Ads

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -5,6 +5,14 @@ import App from './App.tsx'
 
 document.title = import.meta.env.VITE_APP_TITLE ?? 'Scholastic Cloud'
 
+if (import.meta.env.VITE_ADS === 'true') {
+  const adsScript = document.createElement('script')
+  adsScript.async = true
+  adsScript.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1770945751834620'
+  adsScript.crossOrigin = 'anonymous'
+  document.head.appendChild(adsScript)
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
